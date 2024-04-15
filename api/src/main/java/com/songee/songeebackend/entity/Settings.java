3eb8e379061ccc30035f636a1118bdae6f913e8a
stanislawgren <stanislawg1200@gmail.com>
2024-04-15 11:53:56 +0200

    added settings table

---
+++ api/src/main/java/com/songee/songeebackend/entity/Settings.java
@@ -0,0 +1,24 @@
+package com.songee.songeebackend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Settings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "spotify", nullable = false)
+    private int spotify;
+
+    @Column(name = "x2fa", nullable = false)
+    private int x2fa;
+
+    @Column(name = "phone", nullable = false)
+    private int phone;
+}
